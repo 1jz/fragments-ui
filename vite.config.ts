d24463ca3714a5fdfb709f44616ebdbf62ec5817
1jz <1jz@users.noreply.github.com>
2022-07-14 00:53:29 -0400

    undo dockerhub building workflow

--- vite.config.ts
+++ vite.config.ts
@@ -5,23 +5,10 @@ import vue from "@vitejs/plugin-vue";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [
-        vue({
-            include: [/\.vue$/, /\.md$/],
-            template: {
-                compilerOptions: {
-                    isCustomElement: (tag) => tag.startsWith("amplify-"),
-                    sourceMap: true,
-                },
-            },
-        }),
-    ],
+    plugins: [vue()],
     resolve: {
         alias: {
             "@": fileURLToPath(new URL("./src", import.meta.url)),
-            "./runtimeConfig": "./runtimeConfig.browser",
-            find: "./runtimeConfig",
-            replacement: "./runtimeConfig.browser",
         },
     },
     server: {
